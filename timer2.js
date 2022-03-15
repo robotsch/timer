d52545552b8721e525bd1cb44c5c49c18bcf71f3
robotsch <christianhumble22@gmail.com>
2022-03-15 16:56:45 -0400

    add timer2 keypress functionality

--- timer2.js
+++ timer2.js
@@ -1,15 +1,21 @@
-// for(const time of process.argv.slice(2)) {
-//   setTimeout(() => {time > 0 ? process.stdout.write('\x07') : null}, Number(time) * 1000)
-// }
-
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
 
 
 stdin.on('data', (key) => {
-  // setTimeout(() => {time > 0 ? process.stdout.write('\x07') : null}, Number(time) * 1000)
+  // Check if key pressed can be casted to a number
+  if (typeof Number(key) === 'number') {
+    // Beep after that number seconds
+    setTimeout(() => {key > 0 ? process.stdout.write('\x07') : null}, Number(key) * 1000)
+  }
+  // Beep if b is pressed
   if (key === 'b') {
+    process.stdout.write('\x07');
+  }
+  // Exit with console.log if ctrl+c is pressed
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!')
     process.exit();
   }
 });
